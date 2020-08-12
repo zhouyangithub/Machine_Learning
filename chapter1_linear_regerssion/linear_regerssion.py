4df263ab32bc78044ce1298ac167edd3dd7ab677
zhouyan <zhouyancancer@foxmail.com>
2020-08-12 10:27:34 +0800

    add drawing cost function & predict function

--- chapter1_linear_regerssion/linear_regerssion.py
+++ chapter1_linear_regerssion/linear_regerssion.py
@@ -11,13 +11,18 @@ def gradient_descent(X,y,iteration = 400,alpha = 0.02):
     theta = np.ones(X.shape[1])
     m = X.shape[0]
     n = X.shape[1]
+    cost = np.zeros(iteration)
     for num in range(iteration):
         for j in range(n):
             theta[j]+=(alpha/m)*np.sum((y-X@theta)*X[:,j])
-    return theta
+        cost[num] = computCost(X,y,theta)
+    return theta,cost
 def featureNormalize(X):
     mu,sigma = X[:,1].mean(),X[:,1].std(ddof = 1)
     X[:,1] = (X[:,1]-mu)/sigma
+    return X,mu,sigma
+def computCost(X,y,theta):
+    return np.sum(y-X@theta)/(2*X.shape[0])
 def drawFigure(X,y,theta):
     plt.scatter(X[:,1],y)
     a = np.column_stack((np.ones(100),np.linspace(X[:,1].min(),X[:,1].max(),100)))
@@ -25,8 +30,15 @@ def drawFigure(X,y,theta):
     plt.plot(a[:,1],b,label = "predict",c = 'r')
     plt.legend()
     plt.show()
+def predict(theta,mu,sigma):
+    x = float(input('please input your data:'))
+    x_t = (x-mu)/sigma
+    return theta[0]+theta[1]*x_t
 X,y = load_data()
-featureNormalize(X)
-theta = gradient_descent(X,y)
+X ,mu,sigma= featureNormalize(X)
+theta,cost = gradient_descent(X,y)
 drawFigure(X,y,theta)
-print(theta)
+plt.plot(range(len(cost)),cost)
+plt.title("Cost")
+plt.show()
+print(predict(theta,mu,sigma))
