10d1a459317098437aabb822160276806bb2798a
zhouyan <zhouyancancer@outlook.com>
2022-02-09 14:35:09 +0800

    finish logistic regerssion

--- logistic_regression.py
+++ logistic_regression.py
@@ -11,14 +11,15 @@ def initializ_params(dims):
 def logistic(X,y,W,b):
     num_train = X.shape[0]
     num_feature = X.shape[1]
-    a = sigmod(np.dot(X,W)+b)
+    a = sigmod(X@W+b)
     cost = -1/num_train * np.sum(y*np.log(a)+(1-y)*np.log(1-a))
-    dW = np.dot(X.T,(a-y))/num_train
+    dW = (X.T@(a-y).reshape(90))/num_train
     db = np.sum(a-y)/num_train
     cost = np.squeeze(cost)
     return a,cost,dW,db
 
 def logistic_train(X,y,learning_rate,epochs):
+    y = y.reshape(1,-1)
     W,b = initializ_params(X.shape[1])
     cost_list = []
     for i in range(epochs):
@@ -39,4 +40,25 @@ def logistic_train(X,y,learning_rate,epochs):
     return cost_list,params,grads
 
 def predict(X,params):
-    y_pred = sigmod(np.dot(X,params["W"])+params["b"])
+    y_pred = sigmod((X@params["W"])+params["b"])
+    y_pred2 = [1 if i >0.5 else 0 for i in y_pred]
+    return y_pred2
+
+import matplotlib.pyplot as plt
+from sklearn.datasets._samples_generator import make_classification
+X,labels = make_classification(n_samples=100,n_features=2,n_redundant=0,n_informative=2,random_state=1,n_clusters_per_class=2)
+rng = np.random.RandomState(2)
+X += 2*rng.uniform(size = X.shape)
+plt.scatter(X[labels == 0,0],X[labels == 0,1])
+plt.scatter(X[labels == 1,0],X[labels == 1,1])
+#plt.show()
+offset = int(X.shape[0]*0.9)
+X_train,y_train = X[:offset],labels[:offset]
+X_test,y_test = X[offset:],labels[offset:]
+y_train = y_train.reshape((-1,1))
+y_test = y_test.reshape((-1,1))
+
+cost_list,params,grads = logistic_train(X_train,y_train,0.01,1000)
+print(params)
+y_pred = predict(X_test,params)
+print(y_pred)
