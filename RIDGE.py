76c0ced61d215e64d0f3264e52c43a68da572ca6
zhouyan <zhouyancancer@outlook.com>
2022-03-12 19:20:27 +0800

    finish ridge regerssion

--- RIDGE.py
+++ RIDGE.py
@@ -1,5 +1,5 @@
 import numpy as np
-def l2_loss(X,y,w,b,alpha):
+def l2_loss(X,y,w,b,alpha = 0.1):
     num_train = X.shape[0]
     num_feature = X.shape[1]
     y_hat = np.dot(X,w)+b
@@ -18,4 +18,22 @@ def ridge_train(X,y,learning_rate = 0.01,epochs = 1000):
     w,b = initialize_params(X.shape[1])
     for i in range(1,epochs):
         y_hat,loss,dw,db = l2_loss(X,y,w,b)
-        
+        w += -learning_rate*dw
+        b += -learning_rate*db
+        loss_his.append(loss)
+        if i % 50 == 0:
+            print('epoch %d loss %f' %(i,loss))
+        params = {"w":w,"b":b}
+        grads = {"dw":dw,"db":db}
+    return loss_his,params,grads
+
+from sklearn.datasets import load_diabetes
+from sklearn.utils import shuffle
+diabetes = load_diabetes()
+data,target = diabetes.data,diabetes.target
+X,y = shuffle(data,target,random_state=13)
+offset = int(X.shape[0]*0.8)
+X_train,y_train = X[:offset],y[:offset]
+X_test,y_test = X[offset:],y[offset:]
+loss_hit,params,grads = ridge_train(X_train,y_train,0.01,30000)
+print(params)
