fcf7d6fbf2f3b58f8b4dccb2016bde7c8baf2b31
zhouyan <zhouyancancer@outlook.com>
2022-06-12 22:09:05 +0800

    t

--- KNN.py
+++ KNN.py
@@ -3,3 +3,6 @@ from collections import Counter
 import matplotlib.pyplot as plt 
 from sklearn import datasets
 from sklearn.utils import shuffle
+
+iris = datasets.load_iris()
+X,y = shuffle(iris.data,iris.target,random_state=13)
